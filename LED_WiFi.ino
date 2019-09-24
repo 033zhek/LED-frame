96936a8ce709c1accc9a558063688ce19a191898
033zhek <34919041+033zhek@users.noreply.github.com>
2019-09-24 20:13:28 +0300

    Add files via upload
    Add change of LED mode(not modes itself)

--- LED_WiFi.ino
+++ LED_WiFi.ino
@@ -4,24 +4,57 @@
 
 #define SSID_ADDRESS 0
 #define PASSWORD_ADDRESS 32
-#define EEPROM_SIZE 96
+#define LED_MODE_ADDRESS 97
+#define EEPROM_SIZE 97
 
 const char main_page[] PROGMEM = R"=====(<!DOCTYPE html>
 <html>
   <style>
+    body {width: 100%;
+          padding: 100px;
+          margin: auto;
+          background-color: white;
+          text-align: center;
+    }
+    button {
+          margin: auto;
+          border: 2px solid #1E90FF;
+          border-radius = 3px;
+          background-color:#6495ED;
+    }
+    link {
+          background-color:#4169E1; 
+    }
   </style>
   <body>
     <h1>Choose led mode</h1>
-    <p><a href="/1"><button class=\"button\">1</button></a></p>
-    <p><a href="/2"><button class=\"button\">2</button></a></p>
-    <p><a href="/3"><button class=\"button\">3</button></a></p>
-    <p><a href="/wifi_page"><button class=\"button\">Set home wifi ssid and password</button></a></p>
+    <p><a href="/1"><button class="button">1</button></a></p>
+    <p><a href="/2"><button class="button">2</button></a></p>
+    <p><a href="/3"><button class="button">3</button></a></p>
+    <p><a href="/wifi_page"><button class="button link">Set home wifi ssid and password</button></a></p>
   </body>
 </html>)=====";
 
 const char wifi_page[] PROGMEM = R"=====(
 <html>
   <style>
+    body {width: %;
+          margin: auto;
+          background-color: white;
+          text-align: center;
+    }
+    form {
+          width: 100%;
+          margin: auto;
+    }
+    button {
+          border: 2px solid #1E90FF;
+          border-radius = 3px;
+          background-color:#6495ED;
+    }
+    link {
+          background-color:#4169E1;
+    }
   </style>
   <body>
     <form action="/post">
@@ -29,13 +62,14 @@ const char wifi_page[] PROGMEM = R"=====(
       Password: <input type="text" name="Password"><br>
       <input type="submit" value="Submit">
     </form><br>
-    <p><a href="/"><button class=\"button\">Go to LED page</button></a></p>
+    <p><a href="/"><button class="button">Go to LED page</button></a></p>
   </body>
 </html>)=====";
 
 WebServer server(80);
 
 // Set these to your desired credentials.
+uint8_t LED_mode = 0;
 const char *my_ssid = "LED_frame";
 const char *my_password = "LEaDtoyourbrightmemories";
 String ssid;
@@ -67,24 +101,39 @@ void handleInput() {
   String s = main_page;
   server.send(200, "text/html", s);
 }
-void handleFirstMode() {}
-void handleSecondMode() {}
-void handleThirdMode() {}
+void handleFirstMode() {
+  EEPROM.writeByte(LED_MODE_ADDRESS, 1);
+  LED_mode = 1;
+  String s = main_page;
+  server.send(200, "text/html", s);
+  }
+void handleSecondMode() {
+  EEPROM.writeByte(LED_MODE_ADDRESS, 2);
+  LED_mode = 2;
+  String s = main_page;
+  server.send(200, "text/html", s);
+  }
+void handleThirdMode() {
+  EEPROM.writeByte(LED_MODE_ADDRESS, 3);
+  LED_mode = 3;
+  String s = main_page;
+  server.send(200, "text/html", s);
+  }
 
 void setup() {
   EEPROM.begin(EEPROM_SIZE);
   Serial.begin(115200);
   Serial.println();
-  Serial.println("Configuring access point...");
+  Serial.println("Configuring access point...")
   delay(3000);
-  // You can remove the password parameter if you want the AP to be open.
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(my_ssid, my_password);
   Serial.println("AP established");
+  LED_mode = EEPROM.readByte(LED_MODE_ADDRESS);
   ssid = EEPROM.readString(SSID_ADDRESS);
   Serial.println("SSID got from eeprom");
   Serial.println(ssid);
-  
+
   if (ssid[0] != '\0') {
     Serial.println("SSID isn't empty");
     if (!WiFi.config(local_IP, gateway, subnet)) {
@@ -122,5 +171,14 @@ void setup() {
 
 void loop() {
   server.handleClient();
+  switch(LED_mode){
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    }
+    
   delay(1);
 }
